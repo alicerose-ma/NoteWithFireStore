e590a388e17f4a5505f116441d18aaf0e3cae783
Alice <alicerose19th@gmail.com>
2020-02-23 23:09:54 +0700

    first commit

---
+++ NoteWithFireStore/NoteWithFireStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteWithFireStore
+//
+//  Created by Ma Alice on 2/23/20.
+//  Copyright © 2020 Ma Alice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
