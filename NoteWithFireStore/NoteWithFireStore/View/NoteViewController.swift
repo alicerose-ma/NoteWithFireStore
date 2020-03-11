210243abf98acbc5e6edaeaef4993dec8fbd35da
Alice <alicerose19th@gmail.com>
2020-03-12 02:13:12 +0700

    search with scope

--- NoteWithFireStore/NoteWithFireStore/View/NoteViewController.swift
+++ NoteWithFireStore/NoteWithFireStore/View/NoteViewController.swift
@@ -9,42 +9,96 @@
 import UIKit
 
 
-class NoteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, Alertable, UISearchBarDelegate {
+class NoteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, Alertable, UISearchResultsUpdating, UISearchBarDelegate {
     
+    let searchController = UISearchController(searchResultsController: nil)
     @IBOutlet weak var noteTableView: UITableView!
-    @IBOutlet weak var searchBar: UISearchBar!
-
+    
     var noteViewModel =  NoteViewModel()
     var setPasscodeViewModel = SetPasscodeViewModel()
     var selectedRow: Int = -1
     
-    var filterNoteList = [NoteData]()
     var allNoteList = [NoteData]()
+    var filteredNoteList = [NoteData]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         noteTableView.dataSource = self
         noteTableView.delegate = self
-        searchBar.delegate = self
-        searchBar.placeholder = "Enter Keywords to search"
         self.title = "Notes"
         setupNavUI()
+        setupSearchController()
     }
     
- 
-//    search bar for filter notes
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        let filterdata = searchText.isEmpty ? allNoteList : allNoteList.filter { ($0.title.range(of: searchText, options: .caseInsensitive) != nil) || ($0.des.range(of: searchText, options: .caseInsensitive) != nil) }
-
-        if !searchText.isEmpty {
-            filterNoteList = filterdata
+    func updateSearchResults(for searchController: UISearchController) {
+         if !searchController.isActive {
+             print("Cancelled")
+             filteredNoteList = allNoteList
+             print(filteredNoteList)
+             noteTableView.reloadData()
+         } else {
+             let searchBar = searchController.searchBar
+             let selectedScope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+             applySearch(searchText: searchController.searchBar.text!,scope: selectedScope)
+         }
+     }
+    
+    func applySearch(searchText: String, scope: String = "All") {
+        if searchController.searchBar.text! == "" {
+            switch scope {
+            case "All":
+                filteredNoteList = allNoteList
+            case "Lock":
+                filteredNoteList = allNoteList.filter { note in
+                    let lockedNote = note.isLocked == true
+                    return lockedNote
+                }
+            case "Unlock":
+                filteredNoteList = allNoteList.filter { note in
+                   let unlockNote = note.isLocked == false
+                   return unlockNote
+                }
+            default:
+                filteredNoteList = allNoteList
+                print("default")
+            }
+            
+            print(filteredNoteList)
         } else {
-            filterNoteList = allNoteList
+            // Filter the results based on the selected filer and search text
+            filteredNoteList = allNoteList.filter { note in
+                var newScope = ""
+                if (note.isLocked == true) {
+                    newScope = "Lock"
+                } else {
+                    newScope = "Unlock"
+                }
+                let lockList = (scope == "All") || (newScope == scope)
+                return lockList && (note.title.lowercased().contains(searchText.lowercased()) || note.des.lowercased().contains(searchText.lowercased()))
+            }
         }
-        noteTableView.reloadData()
+        
+        self.noteTableView.reloadData()
     }
-
-     
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        applySearch(searchText: searchController.searchBar.text!,scope: searchBar.scopeButtonTitles![selectedScope])
+    }
+    
+    
+    //    search bar for filter notes
+    //    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    //        let filterdata = searchText.isEmpty ? allNoteList : allNoteList.filter { ($0.title.range(of: searchText, options: .caseInsensitive) != nil) || ($0.des.range(of: searchText, options: .caseInsensitive) != nil) }
+    //
+    //        if !searchText.isEmpty {
+    //            filterNoteList = filterdata
+    //        } else {
+    //            filterNoteList = allNoteList
+    //        }
+    //        noteTableView.reloadData()
+    //    }
+    
+    
     func setupNavUI() {
         let addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
         self.navigationItem.rightBarButtonItem = addBtn
@@ -53,10 +107,23 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.navigationItem.leftBarButtonItem = exitBtn
     }
     
+    func setupSearchController(){
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        noteTableView.tableHeaderView = searchController.searchBar
+        
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.scopeButtonTitles = ["All", "Lock", "Unlock"]
+        
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Enter Keywords to search"
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         didLogin()
-        searchBar.text = ""
+        searchController.searchBar.text = nil
     }
     
     //    check if user login before
@@ -72,8 +139,8 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
     //    load note list of user
     func loadNoteList() {
         noteViewModel.getNoteList(completion: { notes in
-            self.filterNoteList = notes
             self.allNoteList = notes
+            self.filteredNoteList = notes
             DispatchQueue.main.async {
                 self.noteTableView.reloadData()
             }
@@ -91,19 +158,19 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return filterNoteList.count
+        //            return filterNoteList.count
+        return filteredNoteList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell =  noteTableView.dequeueReusableCell(withIdentifier: "NoteTableViewCell", for: indexPath) as! NoteTableViewCell
-            if (filterNoteList[indexPath.row].isLocked == false) {
-                cell.titleLabel.text = filterNoteList[indexPath.row].title
-                cell.desLabel.text = filterNoteList[indexPath.row].des
-            } else {
-                cell.titleLabel.text = filterNoteList[indexPath.row].title
-                cell.desLabel.text = "locked"
-            }
-        
+        if (filteredNoteList[indexPath.row].isLocked == false) {
+            cell.titleLabel.text = filteredNoteList[indexPath.row].title
+            cell.desLabel.text = filteredNoteList[indexPath.row].des
+        } else {
+            cell.titleLabel.text = filteredNoteList[indexPath.row].title
+            cell.desLabel.text = "locked"
+        }
         return cell
     }
     
@@ -126,24 +193,24 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete", handler: {(contextualAction, view, boolValue) in
             
             //            check lock status to delete, no lock => delete, locked => input pass to delete
-                if (self.filterNoteList[indexPath.row].isLocked == false) {
-//                    delete on firebase
-                    self.noteViewModel.deleteNote(uniqueID: self.filterNoteList[indexPath.row].id, completion: { message in
-                        print(message)
-                    })
-
-//                    delete noteList UI
-                    for note in self.allNoteList {
-                        if note.id == self.filterNoteList[indexPath.row].id {
-                            self.allNoteList.removeAll{$0.id == note.id}
-                        }
+            if (self.filteredNoteList[indexPath.row].isLocked == false) {
+                //                    delete on firebase
+                self.noteViewModel.deleteNote(uniqueID: self.filteredNoteList[indexPath.row].id, completion: { message in
+                    print(message)
+                })
+                
+                //                    delete noteList UI
+                for note in self.allNoteList {
+                    if note.id == self.filteredNoteList[indexPath.row].id {
+                        self.allNoteList.removeAll{$0.id == note.id}
                     }
-//                    delete filter UI
-                    self.filterNoteList.remove(at: indexPath.row)
-                    self.noteTableView.deleteRows(at: [indexPath], with: .fade)
-                } else {
-                    self.setPasscodeViewModel.getUserPasscode(completion: { passcode in
-                        self.enterPasscodeToDelete(passcode: passcode, indexPath: indexPath)
+                }
+                //                    delete filter UI
+                self.filteredNoteList.remove(at: indexPath.row)
+                self.noteTableView.deleteRows(at: [indexPath], with: .fade)
+            } else {
+                self.setPasscodeViewModel.getUserPasscode(completion: { passcode in
+                    self.enterPasscodeToDelete(passcode: passcode, indexPath: indexPath)
                 })
             }
             self.noteTableView.reloadData()
@@ -166,15 +233,15 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
             if let password = alert.textFields?.first?.text {
                 print("Your password: \(password)")
                 if password == passcode {
-                    self.noteViewModel.deleteNote(uniqueID: self.filterNoteList[indexPath.row].id, completion: { message in
+                    self.noteViewModel.deleteNote(uniqueID: self.filteredNoteList[indexPath.row].id, completion: { message in
                         print(message)
                     })
                     for note in self.allNoteList {
-                        if note.id == self.filterNoteList[indexPath.row].id {
+                        if note.id == self.filteredNoteList[indexPath.row].id {
                             self.allNoteList.removeAll{$0.id == note.id}
                         }
                     }
-                    self.filterNoteList.remove(at: indexPath.row)
+                    self.filteredNoteList.remove(at: indexPath.row)
                     self.noteTableView.deleteRows(at: [indexPath], with: .fade)
                 } else {
                     print("incorrect passcode")
@@ -189,17 +256,17 @@ class NoteViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddNewNote" {
             let destinationVC  = segue.destination as! CreateNoteViewController
-            destinationVC.uniqueID = noteViewModel.createNewUniqueNoteID(noteList: filterNoteList)
+            destinationVC.uniqueID = noteViewModel.createNewUniqueNoteID(noteList: allNoteList)
         }
         
         if segue.identifier == "EditNote" {
-                let destinationVC  = segue.destination as! NoteDetailViewController
-                selectedRow = noteTableView.indexPathForSelectedRow!.row
-                let uniqueID = filterNoteList[selectedRow].id
-                destinationVC.uniqueID = uniqueID
-                let lockStatus = filterNoteList[selectedRow].isLocked
-                destinationVC.lockStatus = lockStatus
-//            }
+            let destinationVC  = segue.destination as! NoteDetailViewController
+            selectedRow = noteTableView.indexPathForSelectedRow!.row
+            let uniqueID = filteredNoteList[selectedRow].id
+            destinationVC.uniqueID = uniqueID
+            let lockStatus = filteredNoteList[selectedRow].isLocked
+            destinationVC.lockStatus = lockStatus
+            //            }
         }
         
         if segue.identifier == "ShowLoginView" {
